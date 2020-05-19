cc16ae9011d336c1413664bd96184ef55cf239ca
davidnso <davidnsoesie1@gmail.com>
2020-05-19 15:20:46 -0400

    prep for deploy part 2

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const path = require("path");
-const express = require("express");
+const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist/cosc412', 'index.html'))
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/cosc412'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname,'/dist/cosc412/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
